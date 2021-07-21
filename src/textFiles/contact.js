30ad2f316b16068a1b1e9ea6d729ef934e013eae
Andrew Zhang <aezhang1@gmail.com>
2021-07-21 16:01:35 -0700

    fixed some colors and text

--- src/textFiles/contact.js
+++ src/textFiles/contact.js
@@ -1,3 +1,8 @@
-const contactText = '<p>Hello!</p><p>This website was created by Andrew Zhang, a rising junior at UC Berkeley studying Electrical Engineering and Computer Science. For more info about me or contact information, here is my email, Github, and LinkedIn:</p><p>Email: aezhang1@gmail.com</p><p>Github: <a href="https://github.com/aezhang">https://github.com/aezhang</a></p><p>LinkedIn: <a href="https://www.linkedin.com/in/aezhang">https://www.linkedin.com/in/aezhang</p>';
+const contactText = `<p>Hello!</p><p>This website was created by Andrew Zhang, 
+a Class of 2023 student at UC Berkeley studying Electrical Engineering and Computer Science. 
+For more info about me or contact information, here is my email, Github, and 
+LinkedIn:</p><p>Email: aezhang1@gmail.com</p><p>Github: <a href="https://github.com/aezhang">
+https://github.com/aezhang</a></p><p>LinkedIn: <a href="https://www.linkedin.com/in/aezhang">
+https://www.linkedin.com/in/aezhang</p>`;
 
 export default contactText;
